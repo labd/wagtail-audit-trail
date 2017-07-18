799de6a122d790efa87ff448ff9db3ecdef1fe4a
Roel Bruggink <roel@jaroel.nl>
2017-07-18 10:04:16 +0200

    Auto set __version__

--- src/wagtail_audit_trail/__init__.py
+++ src/wagtail_audit_trail/__init__.py
@@ -1,2 +1,5 @@
-__version__ = '0.4.0'
+import pkg_resources
+
+__version__ = pkg_resources.get_distribution("wagtail_audit_trail").version
+
 default_app_config = 'wagtail_audit_trail.apps.WagtailAuditTrailConfig'
